09259329e52310b56e20df5871899512e83b9f4f
David Oguns <david.oguns@six3systems.com>
2013-03-26 11:36:43 -0400

    Removing primes code.  Not needed for this solution.

--- problem003/main.cpp
+++ problem003/main.cpp
@@ -2,6 +2,7 @@
 //Project Euler Problem 03
 
 #include <iostream>
+#include <cmath>
 #include "primes.h"
 
 using std::cout;
@@ -9,11 +10,21 @@ using std::endl;
 
 int main(int argc, char **argv)
 {
-  unsigned long long TO_FACTOR = 600851475143;
+  unsigned long long const TO_FACTOR = 600851475143ll;
+  unsigned long long number  = TO_FACTOR; //this will change, and be left with the largest prime factor
+  unsigned long long terminate = TO_FACTOR/2; //starting point of when to terminate
 
-  Primes::init_set();
-  
-  Primes::populateUpToSizeLimit(20000);
-  Primes::populateUpToNumberLimit(200);
-  Primes::print_primes();
+  for(unsigned long long factor = 2; factor < terminate ; ++factor)
+  { //could probably skip a few steps eh?
+    if((number%factor)==0)
+    {
+      while((number%factor)==0)
+      {
+        number /= factor;
+        terminate = static_cast<unsigned long long>(std::ceil(number/static_cast<float>(factor)));
+      }
+    }
+  }
+
+  cout << "Solution: " << number << endl;
 }
