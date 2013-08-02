80630b83655bdc38df577a4bf0207ecc4fe7c8ad
David Oguns <david.oguns@six3systems.com>
2013-08-02 17:03:37 -0400

    Added problem 5...surprisingly easy.

---
+++ problem005/main.cpp
@@ -0,0 +1,29 @@
+//David Oguns
+//Project Euler Problem 05
+
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <numeric>
+#include <tuple>
+#include <set>
+#include <list>
+#include <iomanip>
+
+using namespace std;  //gettin lazy...too much std stuff
+
+int main(int argc, char **argv)
+{
+  unsigned long long sum = 0ll;
+  unsigned long long sqSum = 0ll;
+
+  for(unsigned long long i = 1; i <= 100; ++i)
+  {
+    sum += i;
+    sqSum += pow<unsigned long long>(i, 2);
+  }
+
+  cout << "Solution: " << fixed << setw(20) << (pow<unsigned long long>(sum, 2) - sqSum) << endl;
+  return 0;
+}
+
