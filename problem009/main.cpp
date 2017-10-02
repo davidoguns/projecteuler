cdd25237a1e9d3555722f20c52d257a5ac8fcdc2
David Oguns <davidoguns@gmail.com>
2017-10-02 12:53:37 -0700

    Adding problem9 solution.

--- problem009/main.cpp
+++ problem009/main.cpp
@@ -12,35 +12,6 @@
 
 using namespace std;  //gettin lazy...too much std stuff
 
-//brute force implementation, doesn't even try to use only primes or a sieve list
-// -- one of these days, i'll actually use this function
-set<tuple<int,int>> factorize(unsigned long long number)
-{
-  unsigned long long terminate = number;
-  set<tuple<int, int>> factors;
-  unsigned long long factor = 2;
-  for(; factor < terminate; ++factor)
-  {
-    if(!(number%factor))  //miss this form of == 0
-    {
-      unsigned int count = 0;
-      while(!(number%factor))
-      {
-        ++count;
-        number /= factor;
-      }
-      factors.insert(tuple<int, int>(factor, count));
-    }
-  }
-  
-  if(number != 1)
-  {
-    factors.insert(tuple<int, int>(number, 1)); //what we're left with is to the first
-  }
-
-  return std::move(factors);
-}
-
 int main(int argc, char **argv)
 {
   unsigned int targetSum = 0;
